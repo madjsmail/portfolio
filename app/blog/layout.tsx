e5dc4b1f698f9bc44936dde090f3ae3f06434ac1
abdelmadjid smail <madjidsmail1998@gmail.com>
2024-06-26 04:57:16 +0100

    feat : setup a blog page mvp XD

---
+++ app/blog/layout.tsx
@@ -0,0 +1,14 @@
+import type { ReactNode } from 'react';
+
+type LayoutProps = {
+    children: ReactNode;
+};
+
+export default function Layout({ children }: LayoutProps) {
+
+    return (
+        <section>
+            {children}
+        </section>
+    );
+}
